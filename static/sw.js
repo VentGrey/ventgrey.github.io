6ca41b89e1aae03cf35fa3d7f8c1915c4b8958e7
VentGrey <omarpurataf@gmail.com>
2023-06-11 21:02:01 -0600

    Better cache management

--- static/sw.js
+++ static/sw.js
@@ -10,6 +10,41 @@ self.addEventListener("message", (event) => {
   }
 });
 
+workbox.precaching.precacheAndRoute([
+   // Páginas principales
+   '/index.html',
+   '/about.html',
+   '/contact.html',
+
+   // Hojas de estilo
+   '/styles/main.css',
+
+   // Scripts
+   '/scripts/main.js',
+
+   // Imágenes
+   '/static/img/laesquinagris.png',
+   '/static/img/profile.jpeg',
+   '/static/favicon.ico',
+   '/static/apple-touch-icon.png',
+]);
+
+// Estrategia Cache First para imágenes
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'image-cache',
+    plugins: [
+      new workbox.expiration.Plugin({
+        // Solo guarda las 60 imágenes más recientes
+        maxEntries: 60,
+        // Limpia la caché después de 30 días
+        maxAgeSeconds: 30 * 24 * 60 * 60,
+      })
+    ],
+  })
+);
+
 workbox.routing.registerRoute(
   new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({
